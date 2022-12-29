9267fc0df5e0ff999b8ccf5f1f490f23c67eb415
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2022-12-29 17:53:13 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const mp4CheckBox = document.getElementById("mp4CheckBox");
+const mp3CheckBox = document.getElementById("mp3CheckBox");
+const mainForm = document.getElementById("mainForm");
+const urlTextBox = document.getElementById("urlTextBox");
+const downloadButton = document.getElementById("downloadButton");
+
+mp4CheckBox.addEventListener("click", () => {
+    mainForm.setAttribute("action", "youtube-downloader-server.charlzk.repl.co/download_mp4");
+    mp3CheckBox.checked = false;
+});
+
+mp3CheckBox.addEventListener("click", () => {
+    mainForm.setAttribute("action", "youtube-downloader-server.charlzk.repl.co/download_mp3");
+    mp4CheckBox.checked = false;
+});
+
+urlTextBox.addEventListener("input", () => {
+    if (urlTextBox.value.includes("youtube.com/watch")) {
+        downloadButton.disabled = false;
+    } else {
+        console.log("This is not a YouTube Video");
+        downloadButton.disabled = true;
+    }
+});
