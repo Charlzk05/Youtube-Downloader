01f0590dc9c5d0eabb1c5ed81d4d0fe50ddddd22
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2022-12-30 22:20:45 +0800

    https://

--- main.js
+++ main.js
@@ -5,12 +5,12 @@ const urlTextBox = document.getElementById("urlTextBox");
 const downloadButton = document.getElementById("downloadButton");
 
 mp4CheckBox.addEventListener("click", () => {
-    mainForm.setAttribute("action", "youtube-downloader-server.charlzk.repl.co/download_mp4");
+    mainForm.setAttribute("action", "https://youtube-downloader-server.charlzk.repl.co/download_mp4");
     mp3CheckBox.checked = false;
 });
 
 mp3CheckBox.addEventListener("click", () => {
-    mainForm.setAttribute("action", "youtube-downloader-server.charlzk.repl.co/download_mp3");
+    mainForm.setAttribute("action", "https://youtube-downloader-server.charlzk.repl.co/download_mp3");
     mp4CheckBox.checked = false;
 });
 
