eef08d4b9112849b9048c437e1ad3436bb3e18e3
Charlzk <104715127+Charlzk05@users.noreply.github.com>
2023-01-20 23:45:36 +0800

    Final!

--- main.js
+++ main.js
@@ -2,22 +2,6 @@ const mp3_button = document.getElementById("mp3_button");
 const mp4_button = document.getElementById("mp4_button");
 const progressBar = document.getElementsByClassName("progressBar")[0];
 
-/*
-function animate() {
-    progressBar.setAttribute("style", "display: block;");
-    progressBar.getElementsByClassName("innerBar")[0].animate({
-        width: "100%"
-    }, 7000);
-    setTimeout(() => {
-        progressBar.getElementsByClassName("innerBar")[0].setAttribute("style", "width: 100%;");
-    }, 7000);
-}
-
-mp3_button.addEventListener("click", () => {
-    animate();
-});
-*/
-
 mp3CheckBox_oninput = (event) => {
     if (event.value.includes("https://youtu.be/") || event.value.includes("https://www.youtu.be/") || event.value.includes("https://youtube.com/watch?v=") || event.value.includes("https://www.youtube.com/watch?v=")) {
         mp3_button.disabled = false;
