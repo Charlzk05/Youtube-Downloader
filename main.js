ed7ff255dba2c661409589e9547580c4d3a1d342
charlzk05 <charliezkiecharlzk@gmail.com>
2023-01-20 10:13:23 +0800

    Remove Bootstrap 5

--- main.js
+++ main.js
@@ -1,24 +1,18 @@
-const mp4CheckBox = document.getElementById("mp4CheckBox");
-const mp3CheckBox = document.getElementById("mp3CheckBox");
-const mainForm = document.getElementById("mainForm");
-const urlTextBox = document.getElementById("urlTextBox");
-const downloadButton = document.getElementById("downloadButton");
+const mp3_button = document.getElementById("mp3_button");
+const mp4_button = document.getElementById("mp4_button");
 
-mp4CheckBox.addEventListener("click", () => {
-    mainForm.setAttribute("action", "https://youtube-downloader-server.charlzk.repl.co/download_mp4");
-    mp3CheckBox.checked = false;
-});
-
-mp3CheckBox.addEventListener("click", () => {
-    mainForm.setAttribute("action", "https://youtube-downloader-server.charlzk.repl.co/download_mp3");
-    mp4CheckBox.checked = false;
-});
-
-urlTextBox.addEventListener("input", () => {
-    if (urlTextBox.value.includes("youtube.com/watch")) {
-        downloadButton.disabled = false;
+mp3CheckBox_oninput = (event) => {
+    if (event.value.includes("https://youtu.be/") || event.value.includes("https://www.youtu.be/") || event.value.includes("https://youtube.com/watch?v=") || event.value.includes("https://www.youtube.com/watch?v=")) {
+        mp3_button.disabled = false;
     } else {
-        console.log("This is not a YouTube Video");
-        downloadButton.disabled = true;
+        mp3_button.disabled = true;
     }
-});
+}
+
+mp4CheckBox_oninput = (event) => {
+    if (event.value.includes("https://youtu.be/") || event.value.includes("https://www.youtu.be/") || event.value.includes("https://youtube.com/watch?v=") || event.value.includes("https://www.youtube.com/watch?v=")) {
+        mp4_button.disabled = false;
+    } else {
+        mp4_button.disabled = true;
+    }
+}
